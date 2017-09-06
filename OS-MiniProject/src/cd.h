85b6b0fce87fe71eaaa4c9f6660321b1fa185ff9
Heet Sankesara <heetsankesara3@gmail.com>
2017-09-06 19:34:57 +0530

    integrate cd command in shell (#3)

--- OS-MiniProject/src/cd.h
+++ OS-MiniProject/src/cd.h
@@ -9,10 +9,7 @@ void cd(char cd_dir[]) {
 
 	// chdir command is used to make cd_dir as the present directory
 	 int n = chdir(cd_dir);
-	 if( n == 0 )
-	 	//If directory exists and is changed succesfully
-		 printf("Current Directory: %s\n",cd_dir);
-	 else
+	 if( n != 0 )
 	 	//If directory doesn't exist
 		 printf("bash: cd: cn: No such file or directory\n");
  }
@@ -21,12 +18,7 @@ void cd(char cd_dir[]) {
 void cd_e(char cd_dir[]) {
  
 	 int n = chdir(cd_dir);
-	 if( n == 0 )
-	 	//If directory exists and is changed succesfully
-		 printf("Current Directory: %s\n",cd_dir);
-	 else
-	 	////If directory doesn't exist
-	 {
+	 if( n != 0 ) {
 		printf("bash: cd: cn: No such file or directory\n");
 		exit(1); // exits with a non zero status
 	 }
